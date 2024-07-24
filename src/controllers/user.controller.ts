974400c05dd570c500a4e270243843138c7bfe39
Gabriel <bielgabrieloliveira77@gmail.com>
2024-07-23 23:36:55 -0300

    feat: Add authentication endpoint to user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,3 +1,14 @@
-const userController = {};
+import { Request, Response } from "express";
+
+const userController = {
+  authenticate: (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    if (username === "admin" && password === "admin") {
+      res.status(200).json({ message: "Authenticated" });
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  },
+};
 
 export default userController;
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import testController from "../controllers/test.controller";
+import userController from "../controllers/user.controller";
 
 const userRouter = express.Router();
 
 userRouter.get("/", (req, res) => {
   testController.get(req, res);
+});
+
+userRouter.post("/authenticate", (req, res) => {
+  userController.authenticate(req, res);
 });
 
 export default userRouter;
