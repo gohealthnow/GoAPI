a414a7307ddd385910499c88868e9eed2c795941
Gabriel <bielgabrieloliveira77@gmail.com>
2024-07-28 06:52:21 -0300

    chore: Refactor user authentication logic in user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,11 +3,8 @@ import { Request, Response } from "express";
 const userController = {
   authenticate: (req: Request, res: Response) => {
     const { username, password } = req.body;
-    if (username === "admin" && password === "admin") {
-      res.status(200).json({ message: "Authenticated" });
-    } else {
-      res.status(401).json({ message: "Unauthorized" });
-    }
+    
+    
   },
 };
 
