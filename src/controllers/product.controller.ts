1c455c086dfe34d74eb0eb1ef025c3c8b0db69ca
Polabiel <bielgabrieloliveira77@gmail.com>
2024-08-12 16:40:30 -0300

    feat: Refactor create method in product controller
    The create method in the product controller has been refactored to improve readability and error handling.

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -5,17 +5,24 @@ const prisma = new PrismaClient();
 
 const ProductController = {
   create: async (req: Request, res: Response) => {
-    const { name, price } = req.body;
+    const { name, price } = req.body as { name: string; price: number };
 
     if (!name) return res.status(404).json({ message: "missing name field!" });
     if (!price)
       return res.status(404).json({ message: "missing price field!" });
 
-    const productexisted = await prisma.product.findUnique({
-      where: name,
-    });
+    const productexisted = await prisma.product
+      .findMany({
+        where: {
+          name: name,
+        },
+      })
+      .then((product) => {
+        logger.logger.info(product);
+        return product;
+      });
 
-    if (productexisted)
+    if (productexisted.length > 0)
       return res.status(409).json({ message: "Product already exists" });
 
     const newProduct = await prisma.product
@@ -33,7 +40,38 @@ const ProductController = {
         return error;
       });
 
-    return res.status(201).json({ newProduct, message: "Product created!" });
+    return res
+      .status(201)
+      .json({ products: newProduct, message: "Product created!" });
+  },
+  listAll: async (req: Request, res: Response) => {
+    const products = await prisma.product.findMany().catch((error) => {
+      logger.logger.error(error);
+      return res.status(404).json({ message: "Error listing products" });
+    });
+
+    if (products.toString.length === 0)
+      return res.status(404).json({ message: "No products found" });
+    return res.status(200).json({ products });
+  },
+  delete: async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    if (!id) return res.status(404).json({ message: "missing id field!" });
+
+    await prisma.product
+      .delete({
+        where: {
+          id: Number(id),
+        },
+      })
+      .then(() => {
+        return res.status(204).send();
+      })
+      .catch((error) => {
+        logger.logger.error(error);
+        return res.status(500).json({ message: { ...error } });
+      });
   },
 };
 
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -4,7 +4,80 @@ import ProductController from "../controllers/product.controller";
 const productRouter = Router();
 
 productRouter.post("/create", (req, res) => {
-  ProductController.create(req,res);
+  /* #swagger.tags = ['Products']
+      #swagger.path = '/product/create'
+      #swagger.description = 'Endpoint para criar um produto.'
+      #swagger.parameters['Product'] = {
+        in: 'body',
+        description: 'Product information.',
+        required: true,
+        schema: {
+            $name: "Product Name",
+            $price: 100.00,
+          },
+        }
+      #swagger.responses[201] = {
+        description: 'Produto criado com sucesso.',
+        schema: {
+          $ref: '#/definitions/Product'
+        }
+      }
+      #swagger.responses[404] = {
+        description: 'Erro ao criar produto.'
+      }
+      #swagger.responses[409] = {
+        description: 'Produto já existe.'
+      }
+      #swagger.responses[500] = {
+        description: 'Erro no servidor.'
+      }
+  */
+  ProductController.create(req, res);
+});
+
+productRouter.get("/", (req, res) => {
+  /* #swagger.tags = ['Products']
+      #swagger.path = '/product/'
+      #swagger.description = 'Endpoint para listar todos os produtos.'
+      #swagger.responses[200] = {
+        description: 'Produtos listados com sucesso.',
+        schema: {
+          $ref: '#/definitions/Product'
+        }
+      }
+      #swagger.responses[404] = {
+        description: 'Erro ao listar produtos.'
+      }
+      #swagger.responses[500] = {
+        description: 'Erro no servidor.'
+      }
+  */
+  ProductController.listAll(req, res);
+});
+
+productRouter.delete("/:id", (req, res) => {
+  /* #swagger.tags = ['Products']
+      #swagger.path = '/product/{id}'
+      #swagger.description = 'Endpoint para deletar um produto.'
+      #swagger.parameters['Product'] = {
+        in: 'params',
+        description: 'Product information.',
+        required: true,
+        schema: {
+            $id: 1,
+          },
+        }
+      #swagger.responses[204] = {
+        description: 'Produto deletado com sucesso.',
+      }
+      #swagger.responses[404] = {
+        description: 'Erro ao deletar produto.'
+      }
+      #swagger.responses[500] = {
+        description: 'Erro no servidor.'
+      }
+  */
+  ProductController.delete(req, res);
 });
 
 export default productRouter;
